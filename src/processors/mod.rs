0b9387012c83ce6ff0727641657cc4dd42196f5b
Patrice Billaut <billaut@proton.me>
2024-09-14 23:27:43 +0200

    feat: add processor selection stub

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -3,3 +3,8 @@ use crate::processors::csv::CsvProcessor;
 use std::path::Path;
 
 pub mod csv;
+
+pub fn detect_processor(_path: &Path) -> impl Processor {
+    // TODO: Logic to pick appropriate processor, e.g. based on MIME type.
+    CsvProcessor::new()
+}
