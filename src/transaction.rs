615aeede0539a23024188bc7b188f7d08c9a8f9a
Patrice Billaut <billaut@proton.me>
2024-09-14 18:46:23 +0200

    refactor: derive default for IDs

--- src/transaction.rs
+++ src/transaction.rs
@@ -2,7 +2,7 @@ use crate::ClientID;
 use std::fmt::{Display, Formatter};
 
 /// A globally unique transaction ID.
-#[derive(serde::Deserialize, Debug, Eq, PartialEq, Clone, Copy, Hash)]
+#[derive(serde::Deserialize, Debug, Eq, PartialEq, Clone, Copy, Hash, Default)]
 pub struct TransactionID(pub u32);
 
 impl Display for TransactionID {
@@ -11,15 +11,8 @@ impl Display for TransactionID {
     }
 }
 
-#[cfg(test)]
-impl Default for TransactionID {
-    fn default() -> Self {
-        Self(0)
-    }
-}
-
-/// A transaction is a financial activity or event where a value is exchanged between two parties. 
-/// In the context of banking or finance, it refers to any movement of funds involving accounts, 
+/// A transaction is a financial activity or event where a value is exchanged between two parties.
+/// In the context of banking or finance, it refers to any movement of funds involving accounts,
 /// typically involving deposits, withdrawals, transfers, or payments.
 #[derive(serde::Deserialize, Debug, PartialEq, Clone, Copy)]
 pub struct Transaction {
